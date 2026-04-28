58c51137a70a744dbd0c6cb1036f55e1dd2d8ead
Gonzalo Barbosa Triviño <gonzalo.barbosat@gmail.com>
2026-04-28 13:26:56 +0200

    Fix typos in server initialization and method calls

--- Day17-Constructors/Day17Constructors.java
+++ Day17-Constructors/Day17Constructors.java
@@ -4,15 +4,15 @@ public class Day17Constructors {
         
         
         // usando el constructor completo - obligatorio pasar todos los datos 
-        Server server1 = new Server("WebSerever-01", 1000, 850, true);
-        Serever server2 = new Server("DBServer-01", 500, 120, true);
+        Server server1 = new Server("WebServer-01", 1000, 850, true);
+        Server server2 = new Server("DBServer-01", 500, 120, true);
         
         //usando el constructor sobrecargado -  solo el nombre 
         Server server3 = new Server("FileServer-01");
         
         server1.printReport();
         System.out.println();
-        serever2.prinReport();
+        server2.printReport();
         System.out.println();
         server3.printReport();
     
